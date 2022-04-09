130f0034c320f464834a0b37b92da32075d471fc
Aleco112 <aleco112@yahoo.fr>
2022-04-09 01:01:29 +0000

    new-one

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,44 +1,41 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			list:[]
+			
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getData:()=>{
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/Aleco112",{
+					method :'GET',
+					redirect:'follow'
+				})
+			
+			
+				.then((response) => response.json())
+				.then((result) => setTodoList(result))
+				.catch((error) => console.log("error", error));
+			
+			
+		},
+		addItem: (item)=>{
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/Aleco112",{
+					method :'GET',
+					redirect:'follow',
+					headers: {
+						"content-type": "application/json"
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
+
+					},
+					body:JSON.stringify([...getStore().list, {label: item,done:false}])
+
+		})
+		.then(respose=>respose.status ===200 ? getActions().getData():"")
+		.catch(error=> console.log ("error,error"))
+	}
+}
 	};
 };
 
